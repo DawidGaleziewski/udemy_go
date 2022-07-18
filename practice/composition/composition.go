f2d213327d08a8bd812cb93e6f480c4f66052aa7
uniters_ngo <uniters-fundation@tutanota.com>
2022-07-18 18:28:57 +0200

    saving notes

--- practice/composition/composition.go
+++ practice/composition/composition.go
@@ -52,14 +52,70 @@ func (Crowbar) PullNail(nailSupply *int, b *Board){
 	fmt.Println("Crowbar: yanked nail out of the board.")
 }
 
+// Contractor carries out the task
 type Contractor struct{}
 
+// Fasten drives the nails into the board
+//  The method requires the user to pass as the first parameter a value that implements the NailDriver interface. This value represents the tool the contractor will use to execute this behavior. Using an interface type for the this parameter allows the user of the API to later create and use different tools without the need for the API to change.
 func (Contractor) Fasten(d NailDriver, nailSupply *int, b *Board){
 	for b.NailsDriven < b.NailsNeeded {
 		d.DriveNail(nailSupply, b)
 	}
 }
 
+func (Contractor) Unfasten(p NailPuller, nailSupply *int, b *Board){
+	for b.NailsDriven > b.NailsNeeded {
+		p.PullNail(nailSupply, b)
+	}
+}
+//The final behavior for a contractor is a method called ProcessBoards which allows the contractor to work on a set of boards at one time:
+func (Contractor Contractor) ProcessBoards(dp NailDrivePuller, nailSupply *int, boards []Board){
+	for i := range boards {
+		board := &boards[i]; // we take address of the board to work at the pointer and mutate those values
+
+		switch {
+			case  board.NailsDriven < board.NailsNeeded:
+					Contractor.Fasten(dp, nailSupply, board)
+			case board.NailsDriven > board.NailsNeeded:
+					Contractor.Unfasten(dp, nailSupply, board)		
+		}
+		
+	}
+}
+
+// Notice that so far Fasten and Unfasten requires requires value that implements only ONE act of behaviour. But process boards requires a value that implements two acts of behaviour.
+
+// Toolbox can contains any number of tools.
+/** When embedding a type inside of another type, it is good to think of the new type as the outer type and the type being embedded as the inner type. This is important because then you can see the relationship that embedding a type creates. **/
+
+type Toolbox struct {
+	NailDriver // We have not embedded a struct type into our Toolbox but two interface types. This means any concrete type value that implements the NailDriver interface can be assigned as the inner type value for the NailDriver embedded interface type.
+	NailPuller
+	nails int
+}
+
 func main(){
+
+	// We specify a slice of boards with information on what each of them needs
+	boards := []Board{
+		// To be removed
+		{NailsDriven: 3},
+		{NailsDriven: 1},
+		{NailsDriven: 5},
+
+		// to be fasten
+		{NailsNeeded: 6},
+		{NailsNeeded: 10},
+		{NailsNeeded: 1},
+	}
+
+	tb := Toolbox{
+		NailDriver: Mallet{},
+		NailPuller: Crowbar{},
+		nails: 10,
+	}
+
+	var c Contractor
+	c.ProcessBoards(&tb )
 	fmt.Println("test")
 }
