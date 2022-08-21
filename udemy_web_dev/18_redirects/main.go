aeb98243216144492cfff589886af9af6162d551
uniters_ngo <uniters-fundation@tutanota.com>
2022-08-21 20:51:06 +0200

    saving notes

--- udemy_web_dev/18_redirects/main.go
+++ udemy_web_dev/18_redirects/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 	// there are couple of redirect codes and they will differ in significant ways
 	// 301 - moved permanently. If we give this code, many browsers will remember this and NEVER hit that servers url again. It will go straignt to the redirected url ALWAYS
@@ -7,4 +12,28 @@ func main() {
 	// 307 temporary redirects - keeps same method
 	// ultimate source for definitive information on anything http protocol related https://www.rfc-editor.org/rfc/rfc7231
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "redirected")
+	})
+
+	http.HandleFunc("/move-perm", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/", http.StatusMovedPermanently)
+	})
+
+	http.HandleFunc("/see-other", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w,r, "/", http.StatusSeeOther)
+	})
+
+	http.HandleFunc("/temp-redirect", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w,r, "/", http.StatusTemporaryRedirect)
+	})
+
+
+	// second way of doing this is by setting up headers on response request
+	http.HandleFunc("/redirect", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Location", "/") // set new location for redirect
+		w.WriteHeader(301) // will set it as permanent moved asset. Browser will cache it and not event try to get the resource next time
+	})
+
+	http.ListenAndServe(":9090", nil)
 }
---
+++ udemy_web_dev/19_cookies/main.go
@@ -0,0 +1,10 @@
+package main
+
+// cookie is a file, that server can use to write to client browser, if its allowed to by the machine
+// cookie will be send to the server on each request by the server
+// cookies are domain specific
+// when using https, all data, including that passed by the cookie and the https params, is encrypted
+// when passing the data by url, if someone was looking by your shoulder they could hajiack the session by getting that id from the url
+func main(){
+
+}
