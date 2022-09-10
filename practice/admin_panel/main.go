50b3a3f108ec7e5c0a6ed3521af92e20663ec344
d.galeziewski <d.galeziewski@rynekpierwotny.pl>
2022-09-10 18:16:43 +0200

    update(user entity): setup user entity to create a insertion of new admin user

--- practice/admin_panel/main.go
+++ practice/admin_panel/main.go
@@ -1,21 +1,108 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
+	"log"
+
 	"net/http"
+	"time"
+
+	"admin_panel/user"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
 )
 
-var templates template.Template
-var tmplError error
+var tpl *template.Template
+var err error
+var db *sql.DB
 
 func init() {
-	templates, tmpError := template.ParseGlob("templates/*.go")
+	tpl, err = template.ParseGlob("templates/*.gohtml")
+	if err != nil {
+		log.Println(err)
+	}
+
 }
 
+const PORT = "8080"
+
 func main() {
+	fmt.Println("server starting at port", PORT)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
+			tpl.ExecuteTemplate(w, "homepage.gohtml", "")
+			return
+		}
 
+	})
+
+	http.HandleFunc("/api/user/auth", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			return
 		}
 	})
+
+	http.HandleFunc("/user/new", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			tpl.ExecuteTemplate(w, "user_new.gohtml", "")
+			return
+		}
+	})
+
+	http.HandleFunc("/api/user", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			r.ParseForm()
+
+			name := r.FormValue("name")
+			email := r.FormValue("email")
+			password := r.FormValue("password")
+
+			fmt.Println("form name", name)
+
+			ID, err := uuid.NewUUID()
+			if err != nil {
+				log.Println(err)
+			}
+
+			user := user.User{
+				ID:           ID,
+				Name:         name,
+				Email:        email,
+				Password:     password,
+				CreationDate: time.Now(),
+			}
+
+			userData, validationErrors, err := user.Register(db)
+
+			if err != nil {
+				log.Println(err)
+				http.Error(w, "internal server error", http.StatusInternalServerError)
+				return
+			}
+
+			if len(validationErrors) > 0 {
+				errMsg := ""
+				for _, value := range validationErrors {
+					errMsg += value + "\n"
+				}
+				http.Error(w, errMsg, http.StatusBadRequest)
+				return
+			}
+
+			err = tpl.ExecuteTemplate(w, "user_detail.gohtml", userData)
+			if err != nil {
+				log.Println(err)
+				http.Error(w, "internal server error", http.StatusInternalServerError)
+				return
+			}
+			return
+		}
+
+		return
+	})
+
+	http.ListenAndServe(":"+PORT, nil)
 }
---
+++ practice/admin_panel/user/user.go
@@ -0,0 +1,126 @@
+package user
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"regexp"
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID           uuid.UUID
+	Name         string
+	Email        string
+	Password     string
+	CreationDate time.Time
+}
+
+func (user User) validate() (validationErrors []string, isValid bool, err error) {
+	const MAX_USER_LEN = 100
+	const MIN_PASSWORD_LEN = 8
+	//const MAX_PASSWORD_LEN = 1000
+
+	if len(user.Name) == 0 {
+		validationErrors = append(validationErrors, "user.Name can't be empty")
+	}
+
+	if len(user.Name) > MAX_USER_LEN {
+		validationErrors = append(validationErrors, fmt.Sprint("user.Name can't be longer then %v", MAX_USER_LEN))
+	}
+
+	if len(user.Email) == 0 {
+		validationErrors = append(validationErrors, "user.Email can't be empty")
+	}
+
+	match, err := regexp.MatchString(".*@.*\\..*", user.Email)
+	if err != nil {
+		log.Println("error parsing email regex")
+		return validationErrors, isValid, err
+	}
+
+	if !match || err != nil {
+		validationErrors = append(validationErrors, "user.Email is not a valid email")
+	}
+
+	if len(user.Password) == 0 {
+		validationErrors = append(validationErrors, "user.Password can't be empty")
+	}
+
+	if len(user.Password) == 0 {
+		validationErrors = append(validationErrors, "user.Password can't be empty")
+	}
+
+	if len(user.Password) < MIN_PASSWORD_LEN {
+		validationErrors = append(validationErrors, fmt.Sprintf("user.Password must be at least %v characters long", MIN_PASSWORD_LEN))
+	}
+
+	isValid = len(validationErrors) == 0
+	return validationErrors, isValid, err
+}
+
+func (user User) Register(db *sql.DB) (dbUserRecord User, validationErrors []string, err error) {
+	var isUserDataValid bool
+	validationErrors, isUserDataValid, err = user.validate()
+
+	if err != nil {
+		log.Println(err)
+		return dbUserRecord, validationErrors, err
+	}
+
+	if !isUserDataValid {
+		log.Println(err)
+		return dbUserRecord, validationErrors, err
+	}
+
+	encryptedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.MinCost)
+
+	if err != nil {
+		log.Println(err)
+		return dbUserRecord, validationErrors, err
+	}
+
+	fmt.Println("before creating user")
+	dbUserRecord = User{
+		ID:           user.ID,
+		Name:         user.Name,
+		Password:     string(encryptedPassword),
+		Email:        user.Email,
+		CreationDate: user.CreationDate,
+	}
+
+	db, err = sql.Open("mysql", "test_mysql:Test123!@tcp(127.0.0.1:3306)/gosql?charset=utf8")
+	if err != nil {
+		log.Println(err)
+	}
+	defer db.Close()
+
+	userInsert := fmt.Sprintf(`
+	INSERT INTO gosql.admin_users (id, name, email, password, creation_date)
+	VALUES (
+		'%v',
+		'%v', 
+		'%v',
+		'%v',
+		'%v'
+	);`, dbUserRecord.ID, dbUserRecord.Name, dbUserRecord.Email, dbUserRecord.Password, dbUserRecord.CreationDate.UTC().Format("2006-01-02 03:04:05"))
+
+	fmt.Println("executing query ", userInsert)
+
+	stmt, err := db.Prepare(userInsert)
+	if err != nil {
+		log.Println(err)
+		return dbUserRecord, validationErrors, err
+	}
+
+	_, err = stmt.Exec()
+	if err != nil {
+		log.Println(err)
+		return dbUserRecord, validationErrors, err
+	}
+
+	return dbUserRecord, validationErrors, err
+}
